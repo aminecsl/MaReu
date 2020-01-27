1f3584bdcbe79e72998c2fee4d7d3b716c650665
Amine Katache <a.katache@gmail.com>
2020-01-27 17:04:12 +0100

    Implemented Unit Tests and first 2 Instrumented Tests

---
+++ app/src/androidTest/java/com/example/mareu/MeetingsListInstrumentedTest.java
@@ -0,0 +1,93 @@
+package com.example.mareu;
+
+import androidx.test.espresso.contrib.PickerActions;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.mareu.controllers.activities.MainActivity;
+import com.example.mareu.utils.RecyclerViewUtils;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Calendar;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class MeetingsListInstrumentedTest {
+
+
+    private MainActivity mMainActivity;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+    @Before
+    public void setUp() {
+        mMainActivity = mActivityRule.getActivity();
+        assertThat(mMainActivity, notNullValue());
+    }
+
+    @Test
+    public void checkIfRecyclerViewIsEmpty() {
+        onView(withId(R.id.meetings_rv)).check(new RecyclerViewUtils.ItemCount(0));
+    }
+
+    @Test
+    public void checkIfAddingNewMeetingIsWorking() {
+        // set date to 10 days ahead
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, 10);
+
+        onView(withId(R.id.floatingActionButton)).perform(click());
+        onView(withId(R.id.new_meeting_subject_input)).perform(typeText("Embauche"));
+        onView(withId(R.id.new_meeting_date_input)).perform(click());
+        // set date to datePicker dialog
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
+                .perform(PickerActions.setDate(
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH)+1,
+                        calendar.get(Calendar.DAY_OF_MONTH)));
+        // click ok on the datePicker dialog
+        onView(withId(android.R.id.button1)).perform(click());
+
+        onView(withId(R.id.new_meeting_time_input)).perform(scrollTo(), click());
+        // set time to timePicker dialog
+        onView(withClassName(Matchers.equalTo(TimePicker.class.getName())))
+                .perform(PickerActions.setTime(
+                        11,
+                        30));
+        // click ok on the timePicker dialog
+        onView(withId(android.R.id.button1)).perform(click());
+        onView(withId(R.id.activity_meeting_emails_input)).perform(scrollTo(), replaceText("abcd@gmail.com"));
+        onView(withId(R.id.activity_meeting_add_chip_btn)).perform(scrollTo(), click());
+        onView(withId(R.id.menu_activity_new_meeting_save)).perform(click());
+        onView(withId(R.id.meetings_rv)).check(new RecyclerViewUtils.ItemCount(1));
+    }
+
+    @Test
+    public void checkIfRemovingMeetingIsWorking() {
+
+    }
+}
---
+++ app/src/test/java/com/example/mareu/MeetingServiceUnitTest.java
@@ -0,0 +1,89 @@
+package com.example.mareu;
+
+import com.example.mareu.di.DI;
+import com.example.mareu.model.Meeting;
+import com.example.mareu.model.MeetingRoom;
+import com.example.mareu.service.MeetingApiService;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(JUnit4.class)
+public class MeetingServiceUnitTest {
+
+
+    private MeetingApiService mService;
+
+    @Before
+    public void setup() {
+
+        mService = DI.getNewInstanceApiService();
+    }
+
+    @Test
+    public void addAndGetMeetingWithSuccess() {
+
+        MeetingRoom room = new MeetingRoom(99, "hello");
+        Meeting mMeeting = new Meeting(room,
+                "07/02/2020",
+                "12:30",
+                "Release",
+                Arrays.asList("antoine@lamzone.com",
+                        "pierre@lamzone.com",
+                        "sophie@lamzone.com",
+                        "claude@lamzone.com"));
+        mService.addMeeting(mMeeting);
+        assertTrue(mService.getMeetings().size() == 1);
+        assertTrue(mService.getMeetings().get(0).equals(mMeeting));
+    }
+
+    @Test
+    public void deleteMeetingWithSuccess() {
+
+        MeetingRoom room = new MeetingRoom(77, "world");
+        Meeting mMeeting = new Meeting(room,
+                "18/03/2020",
+                "14:00",
+                "Pause",
+                Arrays.asList("sam@lamzone.com",
+                        "louis@lamzone.com",
+                        "pascal@lamzone.com",
+                        "julia@lamzone.com"));
+        mService.addMeeting(mMeeting);
+        assertTrue(mService.getMeetings().size() == 1);
+        mService.deleteMeeting(mMeeting);
+        assertTrue(mService.getMeetings().size() == 0);
+    }
+
+    @Test
+    public void getFilteredMeetingsWithSuccess() {
+
+        MeetingRoom room1 = new MeetingRoom(55, "a");
+        MeetingRoom room2 = new MeetingRoom(44, "b");
+        Meeting mMeeting1 = new Meeting(room1,
+                "25/04/2020",
+                "13:00",
+                "test1",
+                Arrays.asList("eric@lamzone.com",
+                        "paul@lamzone.com"));
+        Meeting mMeeting2 = new Meeting(room2,
+                "03/02/2020",
+                "16:00",
+                "2",
+                Arrays.asList("jean@lamzone.com",
+                        "carole@lamzone.com"));
+        mService.addMeeting(mMeeting1);
+        mService.addMeeting(mMeeting2);
+        List<Meeting> filteredMeeting = mService.getFilteredMeetingsList("03/02/2020", "b");
+        assertEquals(mMeeting2, filteredMeeting.get(0));
+    }
+
+}
